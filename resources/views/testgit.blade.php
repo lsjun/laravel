eb2d4e93110ecd76e8cdcbc1e48027cdbe38d66c
lsjun <574755781@qq.com>
2016-03-02 18:20:32 +0800

    test

--- resources/views/testgit.blade.php
+++ resources/views/testgit.blade.php
@@ -4,12 +4,25 @@
     <title>github.test page</title>
     {{--<link rel="stylesheet" type="text/css" href="../../public/css/testgit.css" />--}}
     <style type="text/css">
+        * {
+            box-sizing: border-box;
+        }
+
         div {
             display: block;
         }
+
         a {
             color: #4078c0;
             text-decoration: none;
+        }
+
+        img {
+            border: 0;
+        }
+
+        body {
+            word-wrap: break-word;
         }
 
         .btn-sign-up {
@@ -134,6 +147,123 @@
             bottom: 0;
             box-sizing: border-box;
         }
+
+        .marketing-section {
+            position: relative;
+            padding-top: 80px;
+            padding-bottom: 80px;
+            font-size: 16px;
+            line-height: 1.5;
+            text-align: center;
+            border-bottom: 1px solid #e5e5e5;
+        }
+
+        .marketing-section-enterprise {
+            max-height: 375px;
+            padding-bottom: 20px;
+            margin-top: auto;
+            overflow: hidden;
+            color: white;
+            text-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+            box-shadow: inset 0 10px 20px rgba(0, 0, 0, 0.1);
+            background-image: linear-gradient(#202d5f, #614381);
+        }
+
+        .marketing-header {
+            margin-bottom: 40px;
+        }
+
+        .marketing-header .btn {
+            padding: 12px 20px;
+            margin-top: 15px;
+            font-size: 18px;
+            font-weight: normal;
+            border-radius: 6px;
+        }
+
+        .marketing-header h1 {
+            margin-top: 0;
+            margin-bottom: 0;
+            font-size: 42px;
+            font-weight: 300;
+        }
+
+        .btn-marketing {
+            margin-bottom: 20px;
+            color: white;
+            text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+            background: #1d6ac8 linear-gradient(#45b3f3, #1d6ac8);
+            border: 0;
+            box-shadow: 0 1px 1px rgba(0, 0, 0, 0.25);
+        }
+
+        .marketing-section-img {
+            display: block;
+            max-width: 980px;
+            margin: 40px auto;
+            border: 1px solid rgba(0, 0, 0, 0.25);
+            border-radius: 5px;
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.25);
+        }
+
+        .marketing-header .lead {
+            margin-bottom: 30px;
+            font-size: 20px;
+            font-weight: 300;
+            color: #555;
+            max-width: 750px;
+        }
+
+        .columns {
+            margin-left: -10px;
+            margin-right: -10px;
+        }
+
+        .marketing-grid {
+            font-size: 14px;
+            display: table;
+        }
+
+        .column {
+            float: left;
+            /*padding: 20px 25px 40px;*/
+        }
+
+        .one-half {
+            width: 50%;
+        }
+
+        .marketing-section p {
+            width: 90%;
+            margin: 0 auto;
+            color: #5a5a5a;
+        }
+
+        .marketing-section h3 {
+            font-size: 21px;
+            font-weight: normal;
+        }
+
+        .marketing-hero-octicon {
+            position: relative;
+            width: 100px;
+            height: 100px;
+            margin: 0 auto 15px;
+            text-align: center;
+            border: solid 1px #e5e5e5;
+            border-radius: 50px;
+        }
+
+        .marketing-hero-octicon .octicon {
+            margin-top: 22px;
+            color: #4078c0
+        }
+
+        .marketing-hero-octicon .octicon-checklist {
+            position: relative;
+            right: -3px
+        }
+
     </style>
 </head>
 <body style="min-width: 1020px;font-size: 15px;color: #333;background-color: #fff;word-wrap: break-word;margin: 0">
@@ -154,7 +284,6 @@
         </div>
     </div>
 </div>
-
 <div role="main" class="main-content">
     <div class="marketing-section-sign-up">
         <div class="container">
@@ -179,7 +308,7 @@
                 <button type="submit" class="btn btn-sign-up btn-block">Sign up for GitHub</button>
                 <p class="text-muted">
                     By clicking "Sign up for GitHub", you agree to our
-                    <a href="https://help.github.com/terms" target="_blank" >terms of
+                    <a href="https://help.github.com/terms" target="_blank">terms of
                         service</a> and
                     <a href="https://help.github.com/privacy" target="_blank">privacy
                         policy</a>. <span
@@ -199,6 +328,96 @@
         </div>
         <div class="marketing-section-depth"></div>
     </div>
+    <div class="marketing-section marketing-section-enterprise">
+        <div class="container marketing-header">
+            <h1>Want to use GitHub on your servers?</h1>
+            <a href="/" class="btn btn-marketing">Learn more about GitHub Enterprise.</a>
+            <img src="https://assets-cdn.github.com/images/modules/home/gh-enterprise-code.png"
+                 class="marketing-section-img">
+        </div>
+        <div class="marketing-section-depth"></div>
+    </div>
+    <div class="marketing-section">
+        <div class="container">
+            <div class="marketing-header">
+                <h1>Why you'll love GitHub.</h1>
+                <p class="lead">
+                    <a href="/">Powerful features</a> to make software development more collaborative.
+                </p>
+            </div>
+            <div class="columns marketing-grid">
+                <div class="one-half column">
+                    <div class="marketing-hero-octicon">
+                        <svg aria-hidden="true" version="1.1" class="octicon octicon-checklist" role="img" height="48"
+                             width="48" viewBox="0 0 16 16" fill="#4078c0">
+                            <path d="M16 8.5L10 14.5 7 11.5l1.5-1.5 1.5 1.5 4.5-4.5 1.5 1.5zM5.7 12.2l0.8 0.8H2c-0.55 0-1-0.45-1-1V3c0-0.55 0.45-1 1-1h7c0.55 0 1 0.45 1 1v6.5l-0.8-0.8c-0.39-0.39-1.03-0.39-1.42 0L5.7 10.8c-0.39 0.39-0.39 1.02 0 1.41zM4 4h5v-1H4v1z m0 2h5v-1H4v1z m0 2h3v-1H4v1z m-1 1h-1v1h1v-1z m0-2h-1v1h1v-1z m0-2h-1v1h1v-1z m0-2h-1v1h1v-1z"></path>
+                        </svg>
+                    </div>
+                    <h3>Creat collaboration starts with communication.</h3>
+                    <p>Review changes, comment on lines of code, report issues, and plan the future of your project with
+                        discussion tools.</p>
+                </div>
+                <div class="one-half column">
+                    <div class="marketing-hero-octicon">
+                        <svg aria-hidden="true" version="1.1" class="octicon octicon-checklist" role="img" height="48"
+                             width="48" viewBox="0 0 16 16" fill="#4078c0">
+                            <path d="M4.75 4.95c0.55 0.64 1.34 1.05 2.25 1.05s1.7-0.41 2.25-1.05c0.34 0.63 1 1.05 1.75 1.05 1.11 0 2-0.89 2-2s-0.89-2-2-2c-0.41 0-0.77 0.13-1.08 0.33C9.61 1 8.42 0 7 0S4.39 1 4.08 2.33c-0.31-0.2-0.67-0.33-1.08-0.33-1.11 0-2 0.89-2 2s0.89 2 2 2c0.75 0 1.41-0.42 1.75-1.05z m5.2-1.52c0.2-0.38 0.59-0.64 1.05-0.64 0.66 0 1.2 0.55 1.2 1.2s-0.55 1.2-1.2 1.2-1.17-0.53-1.19-1.17c0.06-0.19 0.11-0.39 0.14-0.59zM7 0.98c1.11 0 2.02 0.91 2.02 2.02s-0.91 2.02-2.02 2.02-2.02-0.91-2.02-2.02S5.89 0.98 7 0.98zM3 5.2c-0.66 0-1.2-0.55-1.2-1.2s0.55-1.2 1.2-1.2c0.45 0 0.84 0.27 1.05 0.64 0.03 0.2 0.08 0.41 0.14 0.59-0.02 0.64-0.53 1.17-1.19 1.17z m10 0.8H1c-0.55 0-1 0.45-1 1v3c0 0.55 0.45 1 1 1v2c0 0.55 0.45 1 1 1h1c0.55 0 1-0.45 1-1v-1h1v3c0 0.55 0.45 1 1 1h2c0.55 0 1-0.45 1-1V12h1v1c0 0.55 0.45 1 1 1h1c0.55 0 1-0.45 1-1V11c0.55 0 1-0.45 1-1V7c0-0.55-0.45-1-1-1zM3 13h-1V10H1V7h2v6z m7-2h-1V9h-1v6H6V9h-1v2h-1V7h6v4z m3-1h-1v3h-1V7h2v3z"></path>
+                        </svg>
+                    </div>
+                    <h3>Friction-less development across teams.</h3>
+                    <p>Work with project collaborators or teams of people in organization accounts to communicate with
+                        ease.</p>
+                </div>
+                <div class="one-half column">
+                    <div class="marketing-hero-octicon">
+                        <svg aria-hidden="true" version="1.1" class="octicon octicon-checklist" role="img" height="48"
+                             width="48" viewBox="0 0 18 16" fill="#4078c0">
+                            <path d="M7 9H5l3-3 3 3H9v5H7V9z m5-4c0-0.44-0.91-3-4.5-3-2.42 0-4.5 1.92-4.5 4C1.02 6 0 7.52 0 9c0 1.53 1 3 3 3 0.44 0 2.66 0 3 0v-1.3H3C1.38 10.7 1.3 9.28 1.3 9c0-0.17 0.05-1.7 1.7-1.7h1.3v-1.3c0-1.39 1.56-2.7 3.2-2.7 2.55 0 3.13 1.55 3.2 1.8v1.2h1.3c0.81 0 2.7 0.22 2.7 2.2 0 2.09-2.25 2.2-2.7 2.2H10v1.3c0.38 0 1.98 0 2 0 2.08 0 4-1.16 4-3.5 0-2.44-1.92-3.5-4-3.5z"></path>
+                        </svg>
+                    </div>
+                    <h3>World’s largest open source community.</h3>
+                    <p>
+                        Share your projects with the world, get feedback, and contribute to
+                        <a href="/">millions of repositories</a>
+                        hosted on GitHub.
+                    </p>
+                </div>
+                <div class="one-half column">
+                    <div class="marketing-hero-octicon">
+                        <svg aria-hidden="true" version="1.1" class="octicon octicon-checklist" role="img" height="48"
+                             width="48" viewBox="0 0 12 16" fill="#4078c0">
+                            <path d="M10 7H6L9 0 0 9h4L1 16 10 7z"></path>
+                        </svg>
+                    </div>
+                    <h3>Do more with powerful integrations.</h3>
+                    <p>
+                        Discover applications and tools that
+                        <a href="/">integrate with GitHub</a>
+                        to help you and your team build software better, together.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="marketing-section">
+        <div class="container">
+            <div class="marketing-header">
+                <h1>Simple collaboration from your desktop</h1>
+                <p class="lead">
+                    GitHub Desktop is a seamless way to contribute to projects on
+                    <a href="/">OS X and Windows operating systems</a>.
+                    Create branches, make changes, craft the perfect commit, and deploy—all
+                    from a native interface.
+                </p>
+                <a href="/" class="btn btn-sign-up" style="border-color: #5ca941">
+                    Find out more
+                </a>
+                <img src="https://assets-cdn.github.com/images/modules/home/desktop-mac.png?v=1"
+                     class="marketing-section-img">
+            </div>
+        </div>
+    </div>
 </div>
+
 </body>
 </html>
